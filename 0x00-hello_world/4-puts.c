b9e2a5ddf1ab02b04b9d98a720aa503a4973b958
messouar <messouar.amine@outlook.com>
2023-10-05 21:38:41 +0000

    Initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Print first string of C project
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
