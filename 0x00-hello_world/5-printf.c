52740952b3237efaf4feffab97b7e05c512fc387
messouar <messouar.amine@outlook.com>
2023-10-05 21:45:43 +0000

    Initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print first string of C project
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+	printfputs("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
