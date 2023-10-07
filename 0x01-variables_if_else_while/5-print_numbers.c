45290d6e50b759d34fea45e1ab05ace2fa3b0a9c
messouar <messouar.amine@outlook.com>
2023-10-07 02:42:30 +0000

    initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: print numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+		printf("%d", i);
+
+	putchar('\n');
+
+	return (0);
+}
