6315054ca370bad2f3664b96552c64a63034df1a
messouar <messouar.amine@outlook.com>
2023-10-07 02:44:55 +0000

    Initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: print numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
