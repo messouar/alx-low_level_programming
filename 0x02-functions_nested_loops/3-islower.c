c69fc2f42f665c9e1304708f2f19ef8a586d2edd
messouar <messouar.amine@outlook.com>
2023-10-11 21:56:15 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * Description: Checks for lowercase character
+ * @c: given character
+ *
+ * Return: 1 if lowercase or 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c <= 97 || c >= 122)
+		return (1)
+	else
+		return (0)
+}
