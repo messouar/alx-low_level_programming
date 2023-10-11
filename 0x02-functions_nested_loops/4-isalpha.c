2b16bb6a5f5b013f6abdcecac67403fae82fd3b0
messouar <messouar.amine@outlook.com>
2023-10-11 22:24:26 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetical character
+ * Description: Checks for alphabetical character
+ * @c: given character
+ *
+ * Return: 1 if lowercase or 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
