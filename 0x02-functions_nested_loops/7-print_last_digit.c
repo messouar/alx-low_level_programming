6f68615761e334bbca8b9bb35465908db38274fe
messouar <messouar.amine@outlook.com>
2023-10-12 00:39:16 +0000

    Refactor function to not use _abswq

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,11 @@
 int print_last_digit(int n)
 {
 	int l = 0;
-	int an = 0;
-
-	an = _abs(n);
-	l = an % 10;
+	
+	if (n < 0)
+		l = (n * -1) % 10;
+	else
+		l = n % 10;
 
 	_putchar (l + '0');
 	return (l);
