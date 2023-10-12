923a8e44ffc6bf437eeb68183e899ce6e48bb657
messouar <messouar.amine@outlook.com>
2023-10-12 00:16:05 +0000

    Initial commit~

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * Description: Prints the last digit of a number by modulo 10
+ * @n: given number
+ *
+ * Return: Last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
