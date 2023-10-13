99544fd5fbe91602dc2d8abbc640600fd6994bd2
messouar <messouar.amine@outlook.com>
2023-10-13 00:37:33 +0000

    Initial commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,11 +2,25 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @:
+ * print_line - Print a line of _
+ * Description: Print _ n times
+ * @n: number of times _ should be printed
  *
- * Return:
+ * Return: void
  */
 
+void print_line(int n)
+{
+	int i = 0;
 
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+
+	_putchar('\n');
+}
