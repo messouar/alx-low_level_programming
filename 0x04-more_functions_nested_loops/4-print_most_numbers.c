5435d39a296833ba9813e47f24921b547dec2371
messouar <messouar.amine@outlook.com>
2023-10-13 00:28:42 +0000

    Initial commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,11 +2,21 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @:
+ * print_most_numbers - Function to print selected numbers
+ * Description: Print sequence from 1 to 9 except 2 and 4
  *
- * Return:
+ * Return: void
  */
 
+void print_most_numbers(void)
+{
+	int i = 0;
 
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
