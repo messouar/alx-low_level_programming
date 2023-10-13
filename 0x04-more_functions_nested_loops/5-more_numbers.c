788aefcdc51d4cf2134a4e6ec34af46dd90bbe19
messouar <messouar.amine@outlook.com>
2023-10-13 01:13:40 +0000

    Initial commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,11 +2,28 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @:
+ * more_numbers - Function to print numbers
+ * Description: Print sequence from 1 to 14 repeat 10 times
  *
- * Return:
+ * Return: void
  */
 
+void more_numbers(void)
+{
+	int i = 0;
+	int c = 0;
 
+	for (c = 0; c < 10; c++)
+	{
+		for (i = 48; i <= 57; i++)
+			_putchar(i);
+
+		for (i = 48; i <= 52; i++)
+		{
+			_putchar(49);
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}
