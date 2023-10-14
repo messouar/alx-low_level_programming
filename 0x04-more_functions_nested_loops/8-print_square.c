f5884c53c1413658d2132c23ce3141acee707c04
messouar <messouar.amine@outlook.com>
2023-10-14 02:00:37 +0000

    Initial commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,11 +2,25 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @:
+ * print_square - print square of #
+ * Description: function that prints a square of # character
+ * @size: dimention of the square
  *
- * Return:
+ * Return: void
  */
 
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+
+		}
+		_putchar('\n');
+	}
+}
