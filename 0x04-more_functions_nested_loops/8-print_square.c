aa63a1a2e23e7bb3b88585acb9aa3023dc364236
messouar <messouar.amine@outlook.com>
2023-10-14 02:11:10 +0000

    Fix null or negative size

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,13 +14,18 @@ void print_square(int size)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
+	if (size > 0)
+	{	
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
 
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
