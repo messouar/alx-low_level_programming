1d2114a753af2d279d244164e6efdc58a1d2f3f4
messouar <messouar.amine@outlook.com>
2023-10-19 00:12:59 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @?: 
+ * print_rev - reverse string
+ * Description: function that reverses a given string
+ * @s: given string to reverse
  *
- * Return: 
+ * Return: void
  */
 
+void print_rev(char *s)
+{
+	char *ptr = NULL;
+	int l = 0;
 
+	ptr = s;
+	l = strlen(s);
+	while (l--)
+	{
+		_putchar(*(ptr + l));
+	}
+	_putchar('\n');
+}
