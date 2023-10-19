6f88888a5d2208cc05869e752b4af8441fd6e4be
messouar <messouar.amine@outlook.com>
2023-10-19 02:36:42 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @?: 
+ * print_array -  prints n elements of an array of integers
+ * Description:  prints n elements of an array separated by comma, followed by
+ * space
+ * @a: given array
+ * @n: number of elements
  *
- * Return: 
+ * Return: void
  */
 
+void print_array(int *a, int n)
+{
+	int i = 0;
 
+	while (i <= n - 1)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+		i++;
+	}
+
+	printf("\n");
+}
