c23ee6eb0b3d3651fecd15cf572aaae2437be26a
messouar <messouar.amine@outlook.com>
2023-10-19 02:20:33 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,24 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @?: 
+ * puts_half - prints half of a string
+ * Description: function that prints the second half of the string
+ * @str: given string
  *
- * Return: 
+ * Return: void
  */
 
+void puts_half(char *str)
+{
+	int i, l = 0;
 
+	l = strlen(str);
+	i = ((l - 1) / 2) + 1;
+
+	while (i <= l - 1)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
