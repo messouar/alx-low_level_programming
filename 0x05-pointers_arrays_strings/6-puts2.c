32269ace745c75609499cfa3da3c3c81566b1977
messouar <messouar.amine@outlook.com>
2023-10-19 01:26:33 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,24 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @?: 
+ * puts2 - print even positions in string
+ * Description: function that prints every other character of a string
+ * @str: given string
  *
- * Return: 
+ * Return: void
  */
 
+void puts2(char *str)
+{
+	int i = 0;
+	int l = 0;
 
+	l = strlen(str);
+	while (i <= l - 1)
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
