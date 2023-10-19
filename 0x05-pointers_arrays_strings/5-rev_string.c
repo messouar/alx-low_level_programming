cca4f93930d8d39d582cc641edb0bdcb0d3fb8ec
messouar <messouar.amine@outlook.com>
2023-10-19 00:49:28 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
- * ? - 
- * Description: 
- * @?: 
+ * rev_string - reverse string
+ * Description: function that reverses actual string not just output
+ * @s: given string
  *
- * Return: 
+ * Return: void
  */
 
+void rev_string(char *s)
+{
+	int l = 0;
+	int i = 0;
+	char temp;
 
+	l = strlen(s);
+
+	while (i <= l / 2)
+	{
+		temp = *(s + i);
+		*(s + i) = *(s + (l - 1));
+		*(s + (l - 1)) = temp;
+
+		i++;
+		l--;
+	}
+}
