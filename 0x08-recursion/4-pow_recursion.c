44e9ad93341d6aab1aab97039ea20c6ac4b2acc6
messouar <messouar.amine@outlook.com>
2023-10-26 23:30:06 +0200

    Initial commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
 /**
- * ? -
- * @?:
+ * _pow_recursion - calculates the value of x raised to the power of y.
+ * @x: root number
+ * @y: raised power
  *
- * Return:
+ * Return: result of power
  */
 
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
 
+	else
+	{
+		if (y == 0)
+			return (1);
+		else
+			return (x * _pow_recursion(x, y - 1));
+	}
+}
