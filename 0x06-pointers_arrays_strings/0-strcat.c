a2c865afa99b4c1e0d485841bac0cd7bbe26e3cc
messouar <messouar.amine@outlook.com>
2023-10-26 02:22:49 +0200

    Initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,36 @@
 #include "main.h"
 
 /**
- * ? - 
- * @?: 
+ * _strcat - concat two strings
+ * @dest: string in first position
+ * @src: string to concat to second position
  *
- * Return: 
+ * Return: pointer to cancatenated string
  */
 
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int l1 = 0;
+	int l2 = 0;
 
+	if (dest == NULL || src == NULL)
+		return (NULL);
+
+	l1 = strlen(dest);
+	l2 = strlen(src);
+
+	i = l1;
+
+	while (j < l2)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
