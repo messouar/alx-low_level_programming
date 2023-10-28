48568446f9f15160f8894acdac57cef3004ea33e
messouar <messouar.amine@outlook.com>
2023-10-28 03:30:50 +0200

    Initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: program that prints its name, followed by a new line.
+ * @argc: number of passed arguments
+ * @argv: pointer to array of strings
+ *
+ * Return: void
+ */
+
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n", argc - 1);
+	return (EXIT_SUCCESS);
+}
