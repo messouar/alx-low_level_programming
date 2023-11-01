b4d3071300b1fe852faef8977076fec945a50094
messouar <messouar.amine@outlook.com>
2023-11-01 23:11:06 +0100

    Initial commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,13 +1,28 @@
 #include "main.h"
 
 /**
- * ? - 
- * @?: 
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
  *
- * Return: ?
+ * Return: pointer to array
  */
 
-char *create_array(unsigned int size, char c)
+int **alloc_grid(int width, int height)
 {
+	int **biDimentionArray;
+	int counter;
 
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	biDimentionArray = calloc(height, sizeof(int));
+
+	for (counter = 0; counter < height; counter++)
+		biDimentionArray[counter] = calloc(width, sizeof(int));
+
+	if (biDimentionArray == NULL)
+		return (NULL);
+
+	return (biDimentionArray);
 }
