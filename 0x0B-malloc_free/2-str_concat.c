7cd35b1fa3e237688c3d1fd337a1c3df6815dba1
messouar <messouar.amine@outlook.com>
2023-11-01 04:11:10 +0100

    Initial commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,13 +1,42 @@
 #include "main.h"
 
 /**
- * ? - 
- * @?: 
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
  *
- * Return: ?
+ * Return: pointer to concatenated string
  */
 
-char *create_array(unsigned int size, char c)
+char *str_concat(char *s1, char *s2)
 {
+	char *concatenatedString = NULL;
+	int s1Length = 0;
+	int s2Length = 0;
+	int counter = 0;
 
+	if (s1 == NULL)
+		s1Length = 0;
+	else
+		s1Length = strlen(s1);
+
+	if (s2 == NULL)
+		s2Length = 0;
+	else
+		s2Length = strlen(s2);
+
+	 concatenatedString = calloc(s1Length + s2Length + 1, sizeof(char));
+
+	 if (concatenatedString == NULL)
+		 return (NULL);
+
+	 for (counter = 0; counter < s1Length; counter++)
+		 concatenatedString[counter] = s1[counter];
+
+	 for (counter = 0; counter < s2Length; counter++)
+          concatenatedString[counter + s1Length] = s2[counter];
+
+	 concatenatedString[counter + s1Length] = '\0';
+
+	 return (concatenatedString);
 }
