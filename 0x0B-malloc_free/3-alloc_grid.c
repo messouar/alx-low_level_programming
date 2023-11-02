43acf1cf5768b76be5bda98ae7be74ae7f9a4783
messouar <messouar.amine@outlook.com>
2023-11-02 03:25:50 +0100

    Fix Free all if allocation fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,42 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **biDimentionArray;
+	int **biDimensionArray;
 	int counter1;
 	int counter2;
+	int allocation_failed = 0;
 
 	if (width == 0 || height == 0)
 		return (NULL);
 
-	biDimentionArray = calloc(height, sizeof(*biDimentionArray));
+	/* Allocate memory for the array of pointers (rows) */
+	biDimensionArray = (int **)malloc(height * sizeof(int *));
 
-	if (biDimentionArray == NULL)
+	if (biDimensionArray == NULL)
 		return (NULL);
 
 	for (counter1 = 0; counter1 < height; counter1++)
 	{
-		biDimentionArray[counter1] = calloc(width, sizeof(**biDimentionArray));
+		/* Allocate memory for each row */
+		biDimensionArray[counter1] = (int *)malloc(width * sizeof(int));
 
-		if (biDimentionArray[counter1] == NULL)
+		if (biDimensionArray[counter1] == NULL)
 		{
-			for (counter2 = 0; counter2 <= counter1; counter2++)
-				free(biDimentionArray[counter2]);
-
-			free(biDimentionArray);
-			return (NULL);
+			allocation_failed = 1;
+			break;
 		}
 	}
 
-	return (biDimentionArray);
+	/* Check if any allocation failed */
+	if (allocation_failed)
+	{
+		for (counter2 = 0; counter2 < counter1; counter2++)
+		{
+			free(biDimensionArray[counter2]);
+		}
+		free(biDimensionArray);
+		return (NULL);
+	}
+
+	return (biDimensionArray);
 }
