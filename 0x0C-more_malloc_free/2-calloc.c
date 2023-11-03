fac742c4bf9ae33d6d33373cd043cbdd8271f1d0
messouar <messouar.amine@outlook.com>
2023-11-03 02:34:27 +0100

    Initial commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,13 +1,25 @@
 #include "main.h"
 
 /**
- * $function_name$ - $function_description$
- * @$argument$: $argument_description$
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements of array
+ * @size: elementary size of 1 unit type
  *
- * Return: $return_type$
+ * Return: pointer to allocated array
  */
 
-$function_name$(type $argument$)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *allocatedArray = NULL;
+	unsigned int counter = 0;
 
+	allocatedArray = malloc(nmemb * size);
+
+	if (allocatedArray == NULL || nmemb == 0 || size == 0)
+		return (NULL);
+
+	for (counter = 0; counter < nmemb; counter++)
+		allocatedArray[counter] = 0;
+
+	return (allocatedArray);
 }
