c74fbd3ea87dccd51422e9a433eb3a58e6030aa7
messouar <messouar.amine@outlook.com>
2023-11-03 02:35:28 +0100

    Initial commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (allocatedArray == NULL || nmemb == 0 || size == 0)
 		return (NULL);
 
-	for (counter = 0; counter < nmemb; counter++)
+	for (counter = 0; counter < (nmemb * size); counter++)
 		allocatedArray[counter] = 0;
 
 	return (allocatedArray);
