2ccc067c11095bdd0cb8c5b10b42771a40dfb7a8
messouar <messouar.amine@outlook.com>
2023-11-03 03:21:08 +0100

    Initial commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,13 +1,34 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * $function_name$ - $function_description$
- * @$argument$: $argument_description$
+ * array_range - function that creates an array of integers.
+ * @min: min value
+ * @max: max value
  *
- * Return: $return_type$
+ * Return: pointer to array
  */
 
-$function_name$(type $argument$)
+int *array_range(int min, int max)
 {
+	int size = 0;
+	int *result = NULL;
+	int i = 0;
 
+	if (min > max)
+		return (NULL);
+
+	if (max - min + 1 > INT_MAX || max - min + 1 < INT_MIN)
+		return (NULL);
+
+	size = max - min + 1;
+	result = malloc(size * sizeof(int));
+
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		result[i] = min + i;
+
+	return (result);
 }
